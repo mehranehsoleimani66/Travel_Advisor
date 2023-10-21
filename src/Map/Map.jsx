607bcd48808d0893318b82600d2ef837953ff4a1
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-21 21:14:09 +0330

    expanded map.jsx

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import { LocationOnOutlined } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+// import useStyles from "./styles.js";
 const Map = () => {
-  return <div>Map</div>;
+  return {};
 };
 
 export default Map;
