8cac019995724827fa1e2ba6ab95b53bc1bf33e9
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-21 10:44:01 +0330

    added header,list,map,placedetails in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./Header/Header";
+import Map from "./Map/Map";
+import List from "./List/List";
+import PlaceDetails from "./PlaceDetails/PlaceDetails";
+import { CssBaseline, Grid } from "@mui/material";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={2} style={{ width: "100%" }}>
+        <Grid item sx={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item sx={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
